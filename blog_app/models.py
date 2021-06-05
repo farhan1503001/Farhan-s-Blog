c0dd9c602ae4deae807d9be04c21796a4ab37f7e
Mohd. Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2021-06-05 23:01:17 +0600

    before creating templates

--- blog_app/models.py
+++ blog_app/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
-    author=models.ForeignKey('auth.User')
+    author=models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title=models.CharField(max_length=200)
     text=models.TextField()
     creation_date=models.DateTimeField(default=timezone.now())
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import mixins
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls.base import reverse_lazy
 from django.utils import timezone
+from django.views.generic.edit import DeleteView
+from blog_app import forms
 from blog_app.models import Post,Comment
 from blog_app.forms import CommentForm,PostForm
-from django.views.generic import (TemplateView,ListView,DetailView)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.views.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
 # Create your views here.
 class AboutView(TemplateView):
     template_name='about.html'
@@ -17,5 +23,67 @@ class PostView(ListView):
 #Now template for viewing specific post
 class PostDetailView(DetailView):
     model=Post
-#Now template for 
+#Now template for createview but we have to ensure the creator has logged in
+class PostCreateView(CreateView,LoginRequiredMixin):
+    login_url='/login/' #Address to the login page
+    redirect_field_name='blog_app/post_detail.html' # After creating where the move will occured
 
+    model=Post
+    form_class=PostForm
+
+#Now template for updating view here it is also required that user is logged in
+class PostUpdateView(UpdateView,LoginRequiredMixin):
+    login_url='/login/'
+    redirect_field_name='blog_app/post_detail.html'
+    
+    model=Post
+    form_class=PostForm
+
+#Now template class for deleting entry
+class PostDeleteView(DeleteView,LoginRequiredMixin):
+    login_url='/login/'
+    success_url=reverse_lazy('post_list')
+
+#Now template class for draft viewing
+class PostDraftView(ListView,LoginRequiredMixin):
+    login_url='/login/'
+    redirect_field_name='blog_app/post_list.html'
+    model=Post
+
+    def get_queryset(self):
+        return Post.objects.filter(publish_date__isnull=True).order_by('-creation_date')
+
+#Now publishing the post
+@login_required
+def post_publish(request,pk=pk):
+    post=get_object_or_404(Post,pk=pk)
+    post.publish()
+    return redirect('post_details',pk=pk)
+
+#########################################################################################################################
+#.......................................Comment Section................................................................#
+#################################################################################################################
+@login_required
+def add_comment(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    if request.method=='POST':
+        form=CommentForm(request.POST)
+        if form.is_valid():
+            comment=form.save(commit=False)
+            comment.post=post
+            comment.save()
+            return redirect('post_detail',pk=post.pk,{'form':form})
+    else:
+        form=CommentForm()
+        return render(request,'blog/comment_form.html')
+
+@login_required
+def approve_comment(request,pk):
+    comment=get_object_or_404(Comment,pk=pk)
+    comment.approve_status()
+    return redirect('post_detail')
+@login_required
+def comment_remove(request,pk):
+    comment=get_object_or_404(Comment,pk=pk)
+    post_pk=comment.post.pk
+    return redirect('post_details',pk=post_pk)
